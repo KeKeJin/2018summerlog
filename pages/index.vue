184701a0ce503a87a5ffe557909693825b89bae8
cssummer18 <cssummer18@cssummer18s-iMac-30.local>
2018-05-22 16:36:29 -0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        2018summerlog
+      </h1>
+      <n/>
+      <h2 class="subtitle">
+        hmc 2018 summer cs startup log
+        <n/>
+      </h2>
+      <div class="links">
+        <n/>
+        <a
+          href="https://docs.google.com/document/d/141XpVpWjHGN_F1DALZi77EVCl3PpdPMhIjaCkNnTf98/edit"
+          target="_blank"
+          class="button--green">daily log</a>
+        <a
+          href="https://docs.google.com/document/d/141XpVpWjHGN_F1DALZi77EVCl3PpdPMhIjaCkNnTf98/edit"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
